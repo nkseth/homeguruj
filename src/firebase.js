26600823cc18529b9cf29f03ebe463940e6df2a9
nkseth <65399445+nkseth@users.noreply.github.com>
2021-05-12 20:37:24 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,7 @@
 import firebase from 'firebase'
 
 var firebaseConfig = {
-    apiKey: "AIzaSyD1mzmFI5TE0YoIOi7CAUK0vN8qo-fM2jA",
-    authDomain: "homeguruji-3edb8.firebaseapp.com",
-    databaseURL: "https://homeguruji-3edb8.firebaseio.com",
-    projectId: "homeguruji-3edb8",
-    storageBucket: "homeguruji-3edb8.appspot.com",
-    messagingSenderId: "52815082332",
-    appId: "1:52815082332:web:268a30e4877b13b4dbc799"
+   
 };
 
 
